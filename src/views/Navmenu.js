60f43e82219ba14c6ac58e834ffe6a99ee949754
abhay1220 <abhay010123@gmail.com>
2025-02-23 14:37:04 -0700

    updated Navmenu.js

--- src/views/Navmenu.js
+++ src/views/Navmenu.js
@@ -1,4 +1,4 @@
-var m = require('mithril')
+var m = require('mithril');
 
 class Link {
   constructor(name, route) {
@@ -10,23 +10,23 @@ class Link {
 module.exports = {
     view: function() {
         var links = [
-            new Link('Home', '/#!'),
+            new Link('Home', '/#!/'),
             new Link('Research', '/#!/research'),
             new Link('Experience', '/#!/experience'),
             new Link('Publications', '/#!/publications'),
         ];
 
-        var items = links.map( function(item) {
-            return m('a', {href: item.route, 
-                class: 'pv1-ns f6 fw6 dim link black mr3 mr3-m mr4-l dib'},
-                item.name)
+        var items = links.map(function(item) {
+            return m('a', {
+                href: item.route,
+                class: 'pv1-ns f6 fw6 dim link black mr3 mr3-m mr4-l dib'
+            }, item.name);
         });
-        
+
         var nav = m('div', {class: 'flex justify-center'}, items);
-        
-        return m('nav', 
-            {class: 'ph3 ph5-ns w-100 bg-near-white pv3 mb3 mb5-ns bt bb b--black-10 overflow-auto'}, 
-            nav
-        ); 
+
+        return m('nav', {
+            class: 'ph3 ph5-ns w-100 bg-near-white pv3 mb3 mb5-ns bt bb b--black-10 overflow-auto'
+        }, nav);
     }
-}
+};
